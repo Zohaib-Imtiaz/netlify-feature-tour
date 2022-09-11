90777d779017ee558656a4f3fe01563e30be7076
MUHAMMAD ZOHAIB IMTIAZ <zohaib_phone@outlook.com>
2022-09-12 00:24:59 +0500

    serverless function commit

---
+++ netlify/functions/hello-world.js
@@ -0,0 +1,15 @@
+// exports.handler = async () => {
+//     return {
+//       statusCode: 200,
+//       body: 'hello world!',
+//     };
+//   };
+
+const handler = async () => {
+    return {
+      statusCode: 200,
+      body: 'hello world!',
+    };
+  };
+
+  export{ handler }
